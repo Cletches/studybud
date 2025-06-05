0f25344fbfae8a7e8f6a78ba24698113ceec04bf
Felix Ofori <99497710+Cletches@users.noreply.github.com>
2025-06-04 22:53:28 -0500

    Update views.py

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def connect(request):
     return render(request, 'connect.html')
