0a004a2f7165c672b7f1637f92ab67aa3e73656c
Felix Ofori <felixofori@Felixs-MacBook-Air-2.local>
2025-06-04 22:07:27 -0500

    Initial commit

---
+++ base/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def connect(request):
+    return render(request, 'connect.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+
+# Create your views here.
